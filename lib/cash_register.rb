2c8e0ab556aa492440642d960bd85a1725686acf
Elijah <bundielly@gmail.com>
2022-08-19 10:16:28 +0300

    commit tree eb35f65d991d96d69b728f1b1532a9684748c862

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
+    attr_accessor :total, :discount
+
+    def initialize(total=0)
+        @total = total
+        @discount = 20
+    end
+
+    def add_item(title, price, quantity=1)
+        self.total += price * quantity
+    end
+end
+
 
